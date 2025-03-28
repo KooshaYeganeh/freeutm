ce86da586fcfa985832c8e75bcb3cc679b1f9285
KooshaYeganeh <kooshakooshadv@gmail.com>
2025-03-29 01:34:16 +0330

    add config File of UTM

---
+++ web/app/config.py
@@ -0,0 +1,15 @@
+# Edit this File With Your Configurations
+# USERNAME = username for Login in App
+USERNAME = "koosha"
+# PASSWORD = Password for Login in App
+PASSWORD = "102030"
+ADMIN_USER="admin"
+ADMIN_PASS="admin"
+# if Your Database Seperated From Application Server You Can Enter ip Address of Database in Remote Server
+
+# Secret Key
+SECRET_KEY = "Koosha Yeganeh Gnu Linux Softwares - KYGnus-Nas "
+
+PORT = 5005 
+
+
